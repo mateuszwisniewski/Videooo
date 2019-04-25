30e77ace62c655eca90bfc081eeef800a1abf41b
Mateusz Wiśniewski <mateusz.wisniewski90@gmail.com>
2019-04-25 18:13:41 +0200

    Update and improve unit tests

---
+++ app/src/test/java/com/wisnia/videooo/network/interceptor/ApiKeyInterceptorTest.kt
@@ -0,0 +1,70 @@
+package com.wisnia.videooo.network.interceptor
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Protocol
+import okhttp3.Request
+import okhttp3.Response
+import org.amshove.kluent.any
+import org.junit.Before
+import org.junit.Test
+
+class ApiKeyInterceptorTest {
+
+    private val apiKeyName = "api_key"
+
+    private val chain = mock<Interceptor.Chain>()
+
+    @Before
+    fun setUp() {
+        whenever(chain.proceed(any(Request::class)))
+            .thenAnswer {
+                Response.Builder()
+                    .code(200)
+                    .protocol(Protocol.HTTP_1_0)
+                    .request(it.arguments[0] as Request)
+                    .message("OK")
+                    .build()
+            }
+    }
+
+    @Test(expected = ApiKeyNotFoundException::class)
+    fun `when intercept with no api key in request query then throw exception`() {
+        // Given
+        val apiKey = ""
+
+        // When
+        val interceptor = ApiKeyInterceptor(apiKey)
+        interceptor.intercept(chain)
+    }
+
+    @Test
+    fun `when intercept with api key in request query then proceed request`() {
+        // Given
+        val apiKey = "example_api_key"
+        addApiKeyQueryParameter(apiKey)
+
+        // When
+        val interceptor = ApiKeyInterceptor(apiKey)
+        interceptor.intercept(chain)
+
+        // Then
+        verify(chain).proceed(any(Request::class))
+    }
+
+    private fun addApiKeyQueryParameter(apiKey: String) {
+        val httpUrlBuilder: HttpUrl.Builder = HttpUrl.Builder()
+            .scheme("http")
+            .host("example-host.com")
+            .addQueryParameter(apiKeyName, apiKey)
+
+        whenever(chain.request()).thenReturn(
+            Request.Builder()
+                .url(httpUrlBuilder.build())
+                .build()
+        )
+    }
+}
---
+++ app/src/test/java/com/wisnia/videooo/repository/token/AuthenticationHeaderExtractorTest.kt
@@ -0,0 +1,36 @@
+package com.wisnia.videooo.repository.token
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.wisnia.domain.authentication.model.Token
+import okhttp3.Headers
+import org.junit.Test
+import retrofit2.Response
+
+private const val AUTHENTICATION_CALLBACK_VALUE = "example_authentication-callback"
+
+class AuthenticationHeaderExtractorTest {
+
+    private val tested = AuthenticationHeaderInterceptor()
+
+    private val tokenResponse = mock<Response<Token>>()
+
+    @Test
+    fun `when send token request then should add authentication header to token`() {
+        whenever(tokenResponse.headers()).thenReturn(headers())
+        whenever(tokenResponse.body()).thenReturn(token())
+
+        // When
+        val observer = tested.intercept(tokenResponse).test()
+
+        // Then
+        observer.assertNoErrors()
+        observer.assertValue { it.authenticationPage == AUTHENTICATION_CALLBACK_VALUE }
+    }
+
+    private fun headers() =
+        Headers.of("Authentication-Callback", AUTHENTICATION_CALLBACK_VALUE)
+
+    private fun token() =
+        Token(true, "example_date", "example_token", null)
+}
